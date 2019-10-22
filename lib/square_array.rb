675024e66fb03ebc0e99e19f173f85a484349ab8
Pedro Huertas <pmh242@gmail.com>
2019-10-22 23:12:37 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,9 @@
 def square_array(numbers)
  	new_array = []
-  counter = 0
+  counter = 0 
   while counter < numbers.length()
   new_array.push(numbers[counter] * numbers[counter])
   counter += 1
-  end
+  end 
   p new_array
 end
